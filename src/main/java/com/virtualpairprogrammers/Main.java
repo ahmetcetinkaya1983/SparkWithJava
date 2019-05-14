e5f4441750d8d43ed45c962573d551979c2f6030
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-05-15 00:22:19 +0300

    Fluent API

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
@@ -28,20 +26,13 @@ public class Main {
 		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
-		JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+		sc.parallelize(inputData)
+		  .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+		  .reduceByKey((value1, value2) -> value1 + value2)
+		  .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
 
-		JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair(rawValue -> {
-			String[] columns = rawValue.split(":");
-			String level = columns[0];
-
-			return new Tuple2<>(level, 1L);
-
-		});
-
-		JavaPairRDD<String, Long> sumsRdd = pairRdd.reduceByKey((value1, value2) -> value1 + value2);
-
-		sumsRdd.foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
-
+		
+		 
 		sc.close();
 
 	}
