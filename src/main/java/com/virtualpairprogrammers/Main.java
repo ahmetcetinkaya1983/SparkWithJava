1b05194589d77faeaf71c07dc904a9bd911cc2ba
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-05-26 15:58:52 +0300

    inMemoryData - working with hardcoded data

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,12 +1,18 @@
 package com.virtualpairprogrammers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-
-import static org.apache.spark.sql.functions.*;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Main {
 
@@ -19,7 +25,25 @@ public class Main {
 												   .config("spark.sql.warehouse.dir", "file:///c:/tmp/")
 												   .getOrCreate();
 		
-		Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+		//Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+		
+		List<Row> inMemory = new ArrayList<>();
+		
+		inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+		inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+		inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+		inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+		inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
+		
+		StructField[] fields = new StructField[]  {
+				new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+				new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+		};
+		
+		StructType schema = new StructType(fields);
+		Dataset<Row> dataset = spark.createDataFrame(inMemory, schema );
+		dataset.show();
+		
 		
 		//Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' AND year >= 2007 ");
 		
@@ -29,10 +53,10 @@ public class Main {
 		//Dataset<Row> modernArtResults = dataset.filter(col("subject").equalTo("Modern Art")
 		//		                                                     .and(col("year").geq(2007)));
 		
-		dataset.createOrReplaceTempView("my_students_table");
-		Dataset<Row> modernArtResults = spark.sql("select * from my_students_table where subject = 'Modern Art' and year >= 2007");
+		//dataset.createOrReplaceTempView("my_students_table");
+		//Dataset<Row> modernArtResults = spark.sql("select * from my_students_table where subject = 'Modern Art' and year >= 2007");
 		
-		modernArtResults.show();
+		//modernArtResults.show();
 		
 
 	}
