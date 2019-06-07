1216312c8ce56b815f1e268194f1768ac6853d9b
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-06-07 18:46:36 +0300

    pivot tables

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,6 +1,7 @@
 package com.virtualpairprogrammers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Level;
@@ -31,19 +32,31 @@ public class Main {
 		
 		//dataset.createOrReplaceTempView("logging_table");
 		
-		/*
+		/* sql grouping 
 		Dataset<Row> results = spark.sql(
 				"select level, date_format(datetime, 'MMMM') as month, count(1) as total "+
 				"from logging_table group by level, month order by cast(first(date_format(datetime, 'M')) as int), level"
 						);
 						*/
 		
+		/* or instead, dataframe grouping
 		dataset = dataset.select(col("level"), 	date_format(col("datetime"),"MMMM").alias("month"), 
 												date_format(col("datetime"),"M").alias("monthnum").cast(DataTypes.IntegerType));
 		
 		dataset = dataset.groupBy(col("level"), col("month"), col("monthnum")).count();
 		dataset = dataset.orderBy(col("monthnum"), col("level"));
 		dataset = dataset.drop(col("monthnum"));
+		*/
+		
+		//or using pivot tables which are more user-friendly
+		dataset = dataset.select(col("level"), 	date_format(col("datetime"),"MMMM").alias("month"), 
+				date_format(col("datetime"),"M").alias("monthnum").cast(DataTypes.IntegerType));
+		
+		Object[] months = new Object[] { "January", "February", "March", "April", "May", "June", "July", "August", "Augcember", "September", "October", "November", "December"};
+		List<Object> columns = Arrays.asList(months);
+ 		
+		
+		dataset = dataset.groupBy("level").pivot("month", columns).count().na().fill(0);;
 		
 		dataset.show(100);
 
